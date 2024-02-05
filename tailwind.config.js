1cfc5e9401861c6a236ef55278ec1dfcd00bcb74
Mahendra0115 <mahendrasing0115@gmail.com>
2024-02-05 23:05:34 +0530

    this is Configration File of Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // all html file acessed by ' * '
+    content: ["./Dist/*.html"],
+
+  // content: ["./Dist/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
